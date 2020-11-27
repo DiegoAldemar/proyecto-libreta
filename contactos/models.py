96e4a6899a5f933716f5645451e15dea5f12fa0e
Diego Aldemar <daldemar@localhost.localdomain>
2020-11-26 21:07:26 -0500

    iniciando proyecto

---
+++ contactos/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Contactos(models.Model):
+    name_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name}  {self.phone_number}  {self.name_user}'
