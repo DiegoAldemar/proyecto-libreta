dea1104fcc9a7d997dff30863cef9158a4eafc27
Diego Aldemar <diegoaldemar95@gmail.com>
2021-01-24 17:06:21 -0500

    configuracion 2

--- mi_libreta/settings.py
+++ mi_libreta/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from secret_key.secret import *
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,8 +137,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 #login required, la redireccion a login para losque no han iniciado sesion
 LOGIN_URL ='/login/'
