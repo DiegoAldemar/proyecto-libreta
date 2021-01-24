6e26c0a63e0fedcfae29723413a7b4613bfcfe80
Diego Aldemar <diegoaldemar95@gmail.com>
2021-01-24 17:09:56 -0500

    configuracion 3

--- mi_libreta/settings.py
+++ mi_libreta/settings.py
@@ -61,7 +61,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMidleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'mi_libreta.urls'
